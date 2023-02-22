3d4f1c90b41d9e04c0ae6c4a39d75254ff448aba
Courtnee McCollum <courtneemeaghan@gmail.com>
2023-02-22 15:41:48 -0600

    Add main methods to posts controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -22,30 +22,118 @@ exports.post_get = (req, res) => {
       res.json(post);
     });
 }
-exports.create_post_post = (req, res) => {
-  res.json('create post post');
-  // User.find({ is_admin: false })
-  //   .exec((err, users) => {
-  //     if (err) return err;
-      
-  //     res.send(users);
-  //   });
-}
-exports.edit_post_post = (req, res) => {
-  res.json('edit post post');
-  // User.find({ is_admin: false })
-  //   .exec((err, users) => {
-  //     if (err) return err;
-      
-  //     res.send(users);
-  //   });
-}
+exports.create_post_post = [
+ body("message", "Message must not be empty.")
+   .trim()
+   .isLength({ min: 1 })
+   .escape(),
+
+ // Process request after validation and sanitization.
+ async (req, res, next) => {
+   // Extract the validation errors from a request.
+   const errors = validationResult(req);
+
+   try {
+     // Create a Post object.
+     const post = new Post({
+       message: req.body.message,
+       author:  req.body.author // res.locals.currentUser._id,
+     });
+
+     if (!errors.isEmpty()) {
+       const errorMessages = errors.errors.map((error) => error.msg);
+       res.json(errorMessages);
+
+       return;
+     }
+
+     // Data from form is valid. Save post.
+     post.save((err) => {
+       if (err) {
+         return next(err);
+       }
+
+       res.json(`post: ${post}`);
+     });
+   } catch (err) {
+     res.json(`error: ${err}`);
+   }
+   return;
+ },
+];
+exports.edit_post_post = [
+  // Validate and sanitize fields.
+  body("message", "Message must not be empty.")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // if (!res.locals.currentUser) res.redirect("/");
+
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Post object.
+    const post = new Post({
+      message: req.body.message,
+      author: req.body.author._id,
+      timestamp: req.body.timestamp,
+      _id: req.params.postId || req.body.postId, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+        const errorMessages = errors.errors.map((error) => error.msg);
+        res.json(errorMessages);
+
+        return;
+    }
+
+    // Data from form is valid. Update the record.
+    Post.findOneAndUpdate({_id: req.params.postId}, post, {}, (err, thepost) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful: redirect to new post record.
+      res.json(`Post edited successfully: ${post}`);
+      // res.redirect("/");
+    });
+
+    return;
+  }
+];
 exports.delete_post_post = (req, res) => {
-  res.json('delete post post');
-  // User.find({ is_admin: false })
-  //   .exec((err, users) => {
-  //     if (err) return err;
-      
-  //     res.send(users);
-  //   });
+   // if (!res.locals.currentUser) res.redirect("/");
+   Post.findOne({_id: req.body.postId})
+   .exec((err, post) => {
+     if (err) return err;
+
+     // Success
+     // Delete object and redirect to the list of categories.
+     Post.deleteOne(post, (err) => {
+       if (err) return err;
+   
+       // Success - go to category list
+       res.json(`Post ${req.params.postId || req.body.postId} successfully deleted`);
+       
+       // res.redirect("/");
+     });
+   });
+
+ Comment.find({ post_id: req.body.postId})
+   .exec((err, comments) => {
+     if (err) return err;
+
+     // Success
+     // Delete object and redirect to the comment's post.
+     comments.forEach((comment) => {
+       Comment.deleteOne(comment, (err) => {
+         if (err) return err;
+     
+       })
+     });
+   });
+
+ return;
 }
