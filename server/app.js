30da22df5100137da37f19ea7cd2cee20f138c5d
Courtnee McCollum <courtneemeaghan@gmail.com>
2023-03-09 16:21:53 -0600

    User profile picture now reflects change after saving if new one is selected

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  // cookie: { maxAge: 1000 * 60 * 60 * 24 * 2 },
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 2 },
 }));
 app.use(passport.initialize());
 app.use(passport.session());
